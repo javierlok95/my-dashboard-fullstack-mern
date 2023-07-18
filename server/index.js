5b4a254caf094276bacf4ea67870599145a65307
javierlok95 <javier.lok.chan@gmail.com>
2023-07-17 21:47:46 -0400

    updated index.js

--- server/index.js
+++ server/index.js
@@ -59,7 +59,7 @@ mongoose
         // Product.insertMany(dataProduct);
         // ProductStat.insertMany(dataProductStat);
         // Transaction.insertMany(dataTransaction);
-        User.insertMany(dataUser);
+        //User.insertMany(dataUser);
 
     })
     .catch((error) => console.log(`${error} did not connect`));
